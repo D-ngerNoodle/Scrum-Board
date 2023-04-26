5ea30a0db34f220a2ff38a004b440ea79046dc6e
Scott Kim <skottcim@gmail.com>
2023-04-26 14:25:21 -0700

    pulled down from dev v2

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -3,6 +3,42 @@ import React, { useState } from 'react';
 export const Project = ({ content, items, setItems, index, id }) => {
   const [toggleTitle, setToggleTitle] = useState(true);
   const [taskTitle, setTaskTitle] = useState(content);
+  // console.log('props.id: ', props.id);
+
+  // deletes specific project from db based on id
+  const deleteItem = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/projects/${id}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        // removes the item from the state
+        setItems(items.filter(el => el.id != id));
+      } else {
+        throw new Error('Failed to delete item from database');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // updates project title
+  const updateName = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/projects/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: id, name: taskTitle }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to update item in database');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="projectContainer">
@@ -26,12 +62,14 @@ export const Project = ({ content, items, setItems, index, id }) => {
             //below vvv
             onBlur={() => {
               setToggleTitle(true);
+              updateName();
             }}
             onKeyDown={event => {
               if (event.key === 'Enter' || event.key === 'Escape') {
                 setToggleTitle(true);
                 event.preventDefault();
                 event.stopPropagation();
+                updateName();
               }
             }}
           />
@@ -40,15 +78,17 @@ export const Project = ({ content, items, setItems, index, id }) => {
           {/* delete button, it works! */}
       <div>
         <button className='deleteButton'
-          onClick={() => {
-            setItems(items.filter(el => el.id != id));
-          }}
+          onClick={deleteItem}
           ><i class="fa fa-trash" style={{ fontSize: '1.5rem' }}></i></button>
         {/* <button className='deleteButton'
           type="button"
-          onClick={() => {
-            setItems(items.filter(el => el.id != id));
-          }}
+          onClick={ 
+          //   () => {
+          //   console.log(id);
+          //   setItems(items.filter(el => el.id != id));
+          // }
+             deleteItem
+          }
         >
           X
         </button> */}
