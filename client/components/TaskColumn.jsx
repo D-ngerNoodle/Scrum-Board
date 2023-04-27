c871a47c76bffd1928c0585e8345612f0a23d41e
Ying-An <yingan.wang@berkeley.edu>
2023-04-27 12:40:24 -0700

    working on dnd bugs

--- client/components/TaskColumn.jsx
+++ client/components/TaskColumn.jsx
@@ -61,19 +61,53 @@ const TaskColumn = ({ taskInfo, name, status}) => {
     console.error(error);
   }
   };
+
+  // handle task drop/status change
+const updateTaskStatus = async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: taskId, status: status }), // this is wrong
+    });
+    if (response.ok) {
+      (renderState ? setRenderState(false) : setRenderState(true));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  const taskSort = list => {
+    const newList = [];
+    for(let el of list){
+      if(el.status === status) newList.push(el);
+    }
+    return newList;
+  }
+
   
   // we need this to render through a component; 
   const taskRender = () => {
     const taskList = [];
-    for(let i = 0; i < userTasks.length; i++){
+    const newArr = taskSort(userTasks);
+    
+    for(let i = 0; i < newArr.length; i++){
       // if the current task status is equal to the specific column status
       // push it to the current column
 
       // error: invalid input syntax for type integer: "taskStatus1"
-
-      if(userTasks[i].status === status){
-   taskList.push(<Task taskName={userTasks[i].task_name} status={userTasks[i].status} id={`taskStatus${userTasks[i].status}`} key={i} taskId={userTasks[i].id}/>);
-      };
+        taskList.push(
+    <Draggable key={newArr[i].id} draggableId={`${newArr[i].id}`} index={i}>
+      {(provided) => (
+        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+          <Task taskName={newArr[i].task_name} status={newArr[i].status} id={`taskStatus${newArr[i].status}`} 
+          key={i} taskId={newArr[i].id}/>
+        </div>
+      )}
+    </Draggable>);
 
     }
     setTasks(taskList);
@@ -81,43 +115,71 @@ const TaskColumn = ({ taskInfo, name, status}) => {
 
   //to run on changes to the second argument (state); needs this to render
   useEffect(() => {
-    setTasks(userTasks);
+    //setTasks(taskSort(userTasks));
     taskRender();
   }, [userTasks]);
 
   // handle add task
 
+  const reorder = (list, startIndex, endIndex) => {
+    console.log(`start:  ${startIndex}, end:  ${endIndex}`);
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
 
+    return result;
+  };
+
+  function onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    console.log(result.source.index)
+    // reorder the list
+    const updatedTasks = reorder(
+      taskSort(userTasks),
+      result.source.index,
+      result.destination.index
+    );
+
+    setTasks(updatedTasks);
+  }
 
   // console.log('tasks is ', tasks);
   return (
-    <div className="column">
-      <div className='columnStatus'>{name}</div>
-      <div className="tasksBox">
-        {tasks}
-      </div>
+    <Droppable droppableId={`column ${status}`} onDragEnd={onDragEnd}>
+      {(provided) => (
+      <div className="column" {...provided.droppableProps} ref={provided.innerRef}>
+        <div className='columnStatus'>{name}</div>
+        <div className="tasksBox">
+          {tasks}
+        </div>
 
-      <div>
-        {/* <button onClick={taskCreator}>New Task</button> */}
-        <Button 
-          className='MUIBUTTON'
-          variant="contained" 
-          sx={{ m: 1.5,
-          boxShadow: 3,
-          fontSize: 20, 
-          background: '#6597a8f6',
-          ":hover": {
-            bgcolor: "#577f8df6",
-          }}}
-          style={{
-            width: '120px',
-            fontSize: '13px'
-          }}
-          onClick={taskCreator}
-        >New Task</Button>
+        <div>
+          {/* <button onClick={taskCreator}>New Task</button> */}
+          <Button 
+            className='MUIBUTTON'
+            variant="contained" 
+            sx={{ m: 1.5,
+            boxShadow: 3,
+            fontSize: 20, 
+            background: '#6597a8f6',
+            ":hover": {
+              bgcolor: "#577f8df6",
+            }}}
+            style={{
+              width: '120px',
+              fontSize: '13px'
+            }}
+            onClick={taskCreator}
+          >New Task</Button>
+        </div>
       </div>
-    </div>
-  );
+      )}
+    </Droppable>
+  )
 };
 
 export default TaskColumn;
